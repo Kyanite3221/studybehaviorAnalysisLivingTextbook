fde636a7c272c16a5fc57169efd42dde40a10b20
kyanite3221 <kqBpIvK*\plIs6kS\I2;*^>
2019-11-08 16:08:10 +0100

    style fixes

--- Main.py
+++ Main.py
@@ -7,19 +7,19 @@ import DataProcessing
 import dateutil.parser
 import Visualisation
 
-USERIDINDEX=0
-SESSIONIDINDEX=1
-TIMESTAMPINDEX=2
-ORIGININDEX=3
-EVENTINDEX=3
-PATHINDEX=4
-CONCEPTINDEXEVENT=4
-INTERNALROUTEINDEX=5
-LEARNINGPATHINDEXEVENTS=5
-STUDYAREAINDEX=6
-LINKINDEX=6
-CONCEPTINDEXPATH=7
-LEARNINGPATHINDEXPATH=8
+USERIDINDEX = 0
+SESSIONIDINDEX = 1
+TIMESTAMPINDEX = 2
+ORIGININDEX = 3
+EVENTINDEX = 3
+PATHINDEX = 4
+CONCEPTINDEXEVENT = 4
+INTERNALROUTEINDEX = 5
+LEARNINGPATHINDEXEVENTS = 5
+STUDYAREAINDEX = 6
+LINKINDEX = 6
+CONCEPTINDEXPATH = 7
+LEARNINGPATHINDEXPATH = 8
 
 
 def datetime_parser(dct):
@@ -34,9 +34,9 @@ def datetime_parser(dct):
     return dct
 
 
-def main(nameFilename,debug=False,fileNames=None,learningPaths={},
+def main(nameFilename, debug=False, fileNames=None, learningPaths={},
          students=None, period=None, heatMapColor="jet", functions=None,
-         filterQuickClicks=False, filesToSave={"metaData":True, "nodes":True},
+         filterQuickClicks=False, filesToSave=None,
          conceptHitsParams=None, heatMapParams=None):
     Visualisation.init(nameFilename)
     settings = {}
@@ -51,7 +51,9 @@ def main(nameFilename,debug=False,fileNames=None,learningPaths={},
 
     if fileNames is None:
         fileNames = [x for x in input("which file would you like to use?(please enter the name of the excel file "
-                                          "or files seperated by commas)\n").split(',')]
+                                      "or files seperated by commas)\n").split(',')]
+    if filesToSave is None:
+        filesToSave = {"metaData": True, "nodes": True}
 
     data = FilterData.filterAndExtractData(fileNames, settings, debug=debug)
     if debug: print("done extracting data")
@@ -59,7 +61,7 @@ def main(nameFilename,debug=False,fileNames=None,learningPaths={},
     Annonymisation.annonymiseExtracted(data)
     if debug: print("done making data anonymous")
 
-    output = DataProcessing.processDataExtracted(data, settings['learningpaths'],filterQuickClicks,
+    output = DataProcessing.processDataExtracted(data, settings['learningpaths'], filterQuickClicks,
                                                  filesToSave=filesToSave)
     # ToDo if visualisation's input is fixed, output['nodes'] can be used instead of loading nodes.json each time
 
@@ -67,7 +69,7 @@ def main(nameFilename,debug=False,fileNames=None,learningPaths={},
         Visualisation.learningpathFlowthrough(settings['learningpaths'][path])
         Visualisation.hitsPerDayPerLearningPath(pathId=path, settings=settings)
         Visualisation.heatMapOfGivenNodes(givenNodes=settings['learningpaths'][path]['list'],
-                                          filename="heatmapPath"+str(path), colors=heatMapColor)
+                                          filename="heatmapPath" + str(path), colors=heatMapColor)
         if debug: print("done for path " + str(path))
     if debug: print(settings)
     DataProcessing.csvExports(nameFilename, learningPaths=settings['learningpaths'], functions=functions)
@@ -76,12 +78,12 @@ def main(nameFilename,debug=False,fileNames=None,learningPaths={},
         if ("all" in functions or "allHitsPerDayPerConceptGraph" in functions) and conceptHitsParams:
             if debug: print("HitsPerDayPerConceptGraph same scale")
             Visualisation.generateSetOfPathVisits(conceptHitsParams, settings=settings,
-                                                  debug=debug, users=False, specificConcept=True) # ToDo this doesn't work yet
+                                                  debug=debug, users=False, specificConcept=True)
         if "HitsPerDayPerConceptGraph" in functions and conceptHitsParams:
             if debug: print("HitsPerDayPerConceptGraph varying scales")
             for conceptId in conceptHitsParams:
                 Visualisation.hitsPerDay(nodeId=conceptId)
-        if "usersPerDayPerLearningPath" in functions: # not all, because if we want all, they should be on the same scale
+        if "usersPerDayPerLearningPath" in functions:  # not all, because if we want all, they should be on the same scale
             if debug: print("usersPerDayPerLearningPath varying scales")
             for path in settings['learningpaths']:
                 Visualisation.usersPerDayPerLearningPath(path, settings=settings)
@@ -91,30 +93,21 @@ def main(nameFilename,debug=False,fileNames=None,learningPaths={},
                                                   debug=debug, users=True)
         if "all" in functions or "allHitsPerDayPerLearningPath" in functions:
             if debug: print("HitsPerDayPerLearningPath same scale")
-            Visualisation.generateSetOfPathVisits(pathId=list(settings['learningpaths']),settings=settings, debug=debug)
+            Visualisation.generateSetOfPathVisits(pathId=list(settings['learningpaths']), settings=settings,
+                                                  debug=debug)
         if "all" in functions or "allNodesFlowthrough" in functions:
             if debug: print("allNodesFlowthrough")
             Visualisation.allNodesFlowthrough(debug=debug)  # ToDo there is currently no way to properly display this
         if ("all" in functions or "allNodesHeatMap" in functions) and heatMapParams:
             if debug: print("allNodesHeatmap")
-            Visualisation.heatMapOfGivenNodes(**heatMapParams) # ToDo this doesn't work yet
+            Visualisation.heatMapOfGivenNodes(**heatMapParams)
 
     # ToDo nodes.json could be saved in such a way that the same nodes.json is not generated twice for the same settings
-    if "all" not in filesToSave: # remove any unwanted files
+    if "all" not in filesToSave:  # remove any unwanted files
         if "metaData" not in filesToSave:
             os.remove("outputs/metaData.json")
         if "nodes" not in filesToSave:
             os.remove("outputs/nodes.json")
-
-    # knownPaths=['8', '135', '136', '144', '143', '142', '141', '140', '148', '145', '137', '139', '138', '146']
-    # Visualisation.generateSetOfPathVisits(knownPaths, metaData=output["metaData"])
-    # Visualisation.learningpathFlowthrough(settings["learningpaths"]['8'])
-    # DataProcessing.csvExports(metaData=output["metaData"], learningPaths=settings["learningpaths"])
-    # for path in knownPaths:
-    #     Visualisation.heatMapOfGivenNodes(givenNodes=settings['learningpaths'][path]['list'],
-    #                                   threshholdFlowthroughValue=2, colors="jet",filename="heatmap" + str(path))
-
-
 
 
 if __name__ == "__main__":
@@ -126,32 +119,32 @@ if __name__ == "__main__":
         period = settings['period']
         fileNames = settings['dataFilename']
         nameFileName = settings['nameFilename']
-        students=settings['students'] if 'students' in settings else None
+        students = settings['students'] if 'students' in settings else None
         heatMapColor = settings['heatMapColor'] if 'heatMapColor' in settings else "jet"
-        debug=settings['debug'] if 'debug' in settings else False
-        functions = dict([(x,True) for x in settings['functions']]) if 'functions' in settings else None
-        filesToSave = settings['filesToSave'] if 'filesToSave' in settings else {"metaData":True, "nodes": True}
+        debug = settings['debug'] if 'debug' in settings else False
+        functions = dict([(x, True) for x in settings['functions']]) if 'functions' in settings else None
+        filesToSave = settings['filesToSave'] if 'filesToSave' in settings else {"metaData": True, "nodes": True}
         heatMapParams = settings['heatMapParams'] if 'heatMapParams' in settings else None
         conceptHitsParams = settings['conceptHitsParams'] if 'conceptHitsParams' in settings else None
         # ToDo use kwargs for this
     else:
         learningpaths = {"8":
-                            {"starting time": "2019-09-03 08:45:00",
+                             {"starting time": "2019-09-03 08:45:00",
                               "list": [11941, 11894, 11833, 11800, 11832, 11801, 11853, 11799, 12070], "id": "8"},
-                        "135":
-                            {"starting time": "2019-09-03 09:30:00",
-                             "list": [12021, 11927, 11835, 11926, 11866, 12038, 11958, 11777, 11980], "id": "135"},
+                         "135":
+                             {"starting time": "2019-09-03 09:30:00",
+                              "list": [12021, 11927, 11835, 11926, 11866, 12038, 11958, 11777, 11980], "id": "135"},
                          }
-        startDate=dateutil.parser.parse("2019-09-01")
-        endDate=dateutil.parser.parse("2019-09-23")
+        startDate = dateutil.parser.parse("2019-09-01")
+        endDate = dateutil.parser.parse("2019-09-23")
         period = {"usePeriod": True, "startDate": startDate, "endDate": endDate}
-        fileNames=["gis_rs_v2_tracking_export3_10_2019.xlsx"]
-        nameFileName="configurationFiles/gis_rs_v2_names.csv"
-        students=None
+        fileNames = ["gis_rs_v2_tracking_export3_10_2019.xlsx"]
+        nameFileName = "configurationFiles/gis_rs_v2_names.csv"
+        students = None
         heatMapColor = "jet"
-        debug=False
+        debug = False
         functions = None
-        filesToSave = {"metaData":True, "nodes": True}
+        filesToSave = {"metaData": True, "nodes": True}
         conceptHitsParams = None
         heatMapParams = None
     main(debug=debug, nameFilename=nameFileName, students=students,
